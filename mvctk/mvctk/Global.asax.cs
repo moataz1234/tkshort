4a712820e2b8359c9b64f41cab6baf44f070824a
moataz1234 <45338957+moataz1234@users.noreply.github.com>
2020-01-05 16:48:39 +0200

    add course and display in admin

--- mvctk/mvctk/Global.asax.cs
+++ mvctk/mvctk/Global.asax.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using mvctk.DAL;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,6 +14,7 @@ namespace mvctk
     {
         protected void Application_Start()
         {
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
