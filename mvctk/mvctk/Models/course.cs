c2885ca561b512d6ba1c74bf61c094c7e16fde8a
moataz1234 <45338957+moataz1234@users.noreply.github.com>
2020-01-05 22:05:13 +0200

    add delete course

--- mvctk/mvctk/Models/course.cs
+++ mvctk/mvctk/Models/course.cs
@@ -10,29 +10,38 @@ namespace mvctk.Models
     public partial class course
     {
         [Key]
+        [Required]
         [StringLength(50)]
         public string ID { get; set; }
 
+        [Required]
         [StringLength(50)]
         public string Name { get; set; }
 
+        [Required]
         public int? Points { get; set; }
 
+        [Required]
         [StringLength(50)]
         public string ExamA { get; set; }
 
+        [Required]
         [StringLength(50)]
         public string ExamB { get; set; }
 
+        [Required]
         [StringLength(50)]
         public string LecturerID { get; set; }
 
+        [Required]
         [StringLength(50)]
         public string Time { get; set; }
-        
+
+        [Required]
         [StringLength(50)]
         public string Day { get; set; }
 
+        [Required]
         [StringLength(50)]
         public string ClassRoom { get; set; }
     }
